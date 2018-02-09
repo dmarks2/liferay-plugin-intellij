e023416efac148f5ae51038ee0f9504a2fa68964
Dominik Marks <d.marks@convotis.com>
2018-02-09 15:58:10 +0100

    Fixed unnecessary Web Facets on parent modules in multi-module projects

--- src/main/java/de/dm/intellij/liferay/language/jsp/LiferayJspWebContentRootListener.java
+++ src/main/java/de/dm/intellij/liferay/language/jsp/LiferayJspWebContentRootListener.java
@@ -4,9 +4,11 @@ import com.intellij.facet.impl.FacetUtil;
 import com.intellij.javaee.web.WebRoot;
 import com.intellij.javaee.web.facet.WebFacet;
 import com.intellij.javaee.web.facet.WebFacetType;
+import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.module.ModuleUtil;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.roots.ModuleRootManager;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.openapi.vfs.VirtualFileEvent;
 import de.dm.intellij.liferay.util.LiferayFileUtil;
@@ -23,6 +25,8 @@ import java.util.List;
  */
 public class LiferayJspWebContentRootListener {
 
+    private static final Logger log = com.intellij.openapi.diagnostic.Logger.getInstance(LiferayJspWebContentRootListener.class.getName());
+
     public static void handleChange(Project project, VirtualFileEvent event) {
         VirtualFile virtualFile = event.getFile();
         if (virtualFile != null) {
@@ -35,27 +39,40 @@ public class LiferayJspWebContentRootListener {
             if (resources != null) {
                 VirtualFile metaInf = LiferayFileUtil.getParent(resources, "META-INF");
                 if (metaInf != null) {
+                    VirtualFile parent = metaInf.getParent();
+
                     final Module module = ModuleUtil.findModuleForFile(virtualFile, project);
 
-                    boolean facetPresent = false;
-                    Collection<WebFacet> webFacets = WebFacet.getInstances(module);
+                    if (module != null) {
 
-                    outer:
-                    for (WebFacet webFacet : webFacets) {
-                        List<WebRoot> webRoots = webFacet.getWebRoots();
-                        for (WebRoot webRoot : webRoots) {
-                            if (webRoot.getFile().equals(resources)) {
-                                facetPresent = true;
-                                break outer;
+                        ModuleRootManager moduleRootManager = ModuleRootManager.getInstance(module);
+
+                        //Add web facet only if at <source-root>/META-INF/resources
+                        for (VirtualFile sourceRoot : moduleRootManager.getSourceRoots()) {
+                            if (sourceRoot.equals(parent)) {
+                                boolean facetPresent = false;
+                                Collection<WebFacet> webFacets = WebFacet.getInstances(module);
+
+                                outer:
+                                for (WebFacet webFacet : webFacets) {
+                                    List<WebRoot> webRoots = webFacet.getWebRoots();
+                                    for (WebRoot webRoot : webRoots) {
+                                        if (webRoot.getFile().equals(resources)) {
+                                            facetPresent = true;
+                                            break outer;
+                                        }
+                                    }
+                                }
+
+                                if (!facetPresent) {
+                                    final WebFacet webFacet = FacetUtil.addFacet(module, WebFacetType.getInstance());
+                                    webFacet.addWebRoot(resources, "/");
+                                }
+
+                                break;
                             }
                         }
                     }
-
-                    if (!facetPresent) {
-                        final WebFacet webFacet = FacetUtil.addFacet(module, WebFacetType.getInstance());
-                        webFacet.addWebRoot(resources, "/");
-                    }
-
                 }
             }
         }
