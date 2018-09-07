333e177363e0e2c7da7e008ad35b69c684a41e76
Dominik Marks <d.marks@convotis.com>
2018-09-07 15:11:53 +0200

    Switched to AlloyUI and Liferay JS to Liferay maven repository versions

--- src/main/java/de/dm/intellij/liferay/language/javascript/AlloyUIPredefinedLibraryProvider.java
+++ src/main/java/de/dm/intellij/liferay/language/javascript/AlloyUIPredefinedLibraryProvider.java
@@ -5,11 +5,16 @@ import com.intellij.openapi.module.Module;
 import com.intellij.openapi.module.ModuleManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VfsUtil;
+import com.intellij.openapi.vfs.VfsUtilCore;
 import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.openapi.vfs.VirtualFileVisitor;
 import com.intellij.webcore.libraries.ScriptingLibraryModel;
 import de.dm.intellij.liferay.module.LiferayModuleComponent;
 import de.dm.intellij.liferay.util.LiferayVersions;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Provides a predefined JavaScript Library for AlloyUI 1.5, 2.0 and 3.0 and Liferay Barebone JS files
@@ -42,12 +47,48 @@ public class AlloyUIPredefinedLibraryProvider extends JSPredefinedLibraryProvide
                     (liferayVersion == LiferayVersions.LIFERAY_VERSION_7_0) ||
                     (liferayVersion == LiferayVersions.LIFERAY_VERSION_UNKNOWN)
                 ) {
+
+            Set<VirtualFile> frontendJsFiles = new HashSet<VirtualFile>();
+            addJavascriptFilesFromDirectory(VfsUtil.findFileByURL(AlloyUIPredefinedLibraryProvider.class.getResource("/com/liferay/js/70/frontend")), frontendJsFiles);
+
+            Set<VirtualFile> frontendJsAuiFiles = new HashSet<VirtualFile>();
+            addJavascriptFilesFromDirectory(VfsUtil.findFileByURL(AlloyUIPredefinedLibraryProvider.class.getResource("/com/liferay/js/70/aui")), frontendJsAuiFiles);
+
             return new ScriptingLibraryModel[] {
-                    ScriptingLibraryModel.createPredefinedLibrary("Alloy UI 3.0", new VirtualFile[]{VfsUtil.findFileByURL(AlloyUIPredefinedLibraryProvider.class.getResource("/alloy-3.0.1/"))}, "http://alloyui.com/api/", true),
-                    ScriptingLibraryModel.createPredefinedLibrary("Liferay 7.0 Barebone Scripts", new VirtualFile[]{VfsUtil.findFileByURL(AlloyUIPredefinedLibraryProvider.class.getResource("/com/liferay/js/70/"))}, true)
+                ScriptingLibraryModel.createPredefinedLibrary("Liferay Frontend JS Web 1.0.79", frontendJsFiles.toArray(new VirtualFile[frontendJsFiles.size()]), true),
+                ScriptingLibraryModel.createPredefinedLibrary("Liferay Frontend JS AUI Web 1.0.33", frontendJsAuiFiles.toArray(new VirtualFile[frontendJsAuiFiles.size()]), true)
             };
         }
 
         return new ScriptingLibraryModel[0];
     }
+
+    private void addJavascriptFilesFromDirectory(VirtualFile directory, final Set<VirtualFile> result) {
+        VfsUtilCore.visitChildrenRecursively(directory,
+            new VirtualFileVisitor() {
+                @Override
+                public boolean visitFile(@NotNull VirtualFile file) {
+                    if (file.isDirectory()) {
+                        return true;
+                    }
+
+                    String extension = file.getExtension();
+                    if ("js".equals(extension)) {
+                        String nameWithoutExtension = file.getNameWithoutExtension();
+                        if (! (
+                            (nameWithoutExtension.endsWith("-min")) ||
+                            (nameWithoutExtension.endsWith("-debug")) ||
+                            (nameWithoutExtension.endsWith("-coverage"))
+                        )) {
+                            result.add(file);
+                        }
+                    }
+
+                    return true;
+                }
+            }
+        );
+
+    }
+
 }
