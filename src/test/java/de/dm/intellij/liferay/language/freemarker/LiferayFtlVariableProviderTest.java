7598f0f51448ac2ab5f282792f60364b261614b7
Dominik Marks <d.marks@xdot.de>
2018-11-09 09:05:58 +0100

    added support for repeatable nested fields in journal structures using freemarker templates

---
+++ src/test/java/de/dm/intellij/liferay/language/freemarker/LiferayFtlVariableProviderTest.java
@@ -0,0 +1,4 @@
+package de.dm.intellij.liferay.language.freemarker;
+
+public class LiferayFtlVariableProviderTest {
+}
---
+++ src/test/java/de/dm/intellij/liferay/language/velocity/LiferayVtlVariableProviderTest.java
@@ -0,0 +1,4 @@
+package de.dm.intellij.liferay.language.velocity;
+
+public class LiferayVtlVariableProviderTest {
+}
