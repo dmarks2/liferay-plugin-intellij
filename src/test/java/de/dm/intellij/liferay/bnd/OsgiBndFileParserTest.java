7dc117fde3afcbdf691e0ab9bd949d83885335ea
Dominik Marks <d.marks@convotis.com>
2019-01-04 11:55:10 +0100

    unit test for bnd file parser

---
+++ src/test/java/de/dm/intellij/liferay/bnd/OsgiBndFileParserTest.java
@@ -0,0 +1,23 @@
+package de.dm.intellij.liferay.bnd;
+
+import com.intellij.testFramework.fixtures.LightCodeInsightFixtureTestCase;
+import de.dm.intellij.liferay.module.LiferayModuleComponent;
+
+public class OsgiBndFileParserTest extends LightCodeInsightFixtureTestCase {
+
+    @Override
+    protected String getTestDataPath() {
+        return "testdata/de/dm/intellij/liferay/bnd/OsgiBndFileParserTest";
+    }
+
+    public void testOsgiBndFileParser() {
+        myFixture.configureByFiles("bnd.bnd");
+
+        //workaround?
+        String text = myFixture.getFile().getText();
+        myFixture.saveText(myFixture.getFile().getVirtualFile(), text);
+
+        assertEquals("com.liferay.dynamic.data.lists.form.web", LiferayModuleComponent.getOsgiFragmentHostPackageName(myFixture.getModule()));
+    }
+
+}
