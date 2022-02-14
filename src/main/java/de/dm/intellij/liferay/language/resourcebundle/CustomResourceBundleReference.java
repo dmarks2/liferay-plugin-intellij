ffbbad71a59e99716b2e439d74b56e52ba24bf76
Dominik Marks <Dominik.Marks@gmx.de>
2022-02-14 08:54:04 +0100

    Feature: Resolve Resource Bundles (only in module scope)

--- src/main/java/de/dm/intellij/liferay/language/resourcebundle/CustomResourceBundleReference.java
+++ src/main/java/de/dm/intellij/liferay/language/resourcebundle/CustomResourceBundleReference.java
@@ -1,10 +1,29 @@
 package de.dm.intellij.liferay.language.resourcebundle;
 
+import com.intellij.codeInsight.completion.PrioritizedLookupElement;
+import com.intellij.codeInsight.lookup.LookupElement;
+import com.intellij.codeInsight.lookup.LookupElementBuilder;
+import com.intellij.icons.AllIcons;
+import com.intellij.lang.properties.PropertiesFileProcessor;
+import com.intellij.lang.properties.PropertiesReferenceManager;
 import com.intellij.lang.properties.PropertiesUtil;
 import com.intellij.lang.properties.ResourceBundleReference;
 import com.intellij.lang.properties.psi.PropertiesFile;
+import com.intellij.openapi.module.Module;
+import com.intellij.openapi.module.ModuleUtil;
+import com.intellij.openapi.roots.ProjectFileIndex;
 import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiElementResolveResult;
 import com.intellij.psi.PsiFile;
+import com.intellij.psi.ResolveResult;
+import com.intellij.psi.search.GlobalSearchScope;
+import com.intellij.util.SmartList;
+import com.intellij.util.containers.ContainerUtil;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class CustomResourceBundleReference extends ResourceBundleReference {
 
@@ -27,5 +46,45 @@ public class CustomResourceBundleReference extends ResourceBundleReference {
         }
 
         return baseName;
+    }
+
+    @Override
+    public ResolveResult @NotNull [] multiResolve(final boolean incompleteCode) {
+        PropertiesReferenceManager referenceManager = PropertiesReferenceManager.getInstance(myElement.getProject());
+        List<PropertiesFile> propertiesFiles = referenceManager.findPropertiesFiles(getResolveScope(), getCanonicalText(), this);
+        return PsiElementResolveResult.createResults(ContainerUtil.map(propertiesFiles, PropertiesFile::getContainingFile));
+    }
+
+    @Override
+    public Object @NotNull [] getVariants() {
+        final ProjectFileIndex projectFileIndex = ProjectFileIndex.SERVICE.getInstance(getElement().getProject());
+        final PropertiesReferenceManager referenceManager = PropertiesReferenceManager.getInstance(getElement().getProject());
+
+        final Set<String> bundleNames = new HashSet<>();
+        final List<LookupElement> variants = new SmartList<>();
+        PropertiesFileProcessor processor = (baseName, propertiesFile) -> {
+            if (!bundleNames.add(baseName)) return true;
+
+            final LookupElementBuilder builder =
+                    LookupElementBuilder.create(baseName)
+                            .withIcon(AllIcons.Nodes.ResourceBundle);
+            boolean isInContent = projectFileIndex.isInContent(propertiesFile.getVirtualFile());
+            variants.add(isInContent ? PrioritizedLookupElement.withPriority(builder, Double.MAX_VALUE) : builder);
+            return true;
+        };
+
+        referenceManager.processPropertiesFiles(getResolveScope(), processor, this);
+        return variants.toArray(LookupElement.EMPTY_ARRAY);
+    }
+
+
+    private GlobalSearchScope getResolveScope() {
+        Module module = ModuleUtil.findModuleForPsiElement(myElement);
+
+        if (module != null) {
+            return GlobalSearchScope.moduleScope(module);
+        }
+
+        return GlobalSearchScope.allScope(myElement.getProject());
     }
 }
--- src/main/java/de/dm/intellij/liferay/language/resourcebundle/ResourceBundleReferenceProvider.java
+++ src/main/java/de/dm/intellij/liferay/language/resourcebundle/ResourceBundleReferenceProvider.java
@@ -1,16 +1,10 @@
 package de.dm.intellij.liferay.language.resourcebundle;
 
-import com.intellij.lang.properties.ResourceBundleReference;
-import com.intellij.lang.properties.psi.PropertiesFile;
-import com.intellij.lang.properties.references.I18nUtil;
-import com.intellij.lang.properties.references.PropertyReference;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiReference;
 import com.intellij.psi.PsiReferenceProvider;
 import com.intellij.util.ProcessingContext;
 import org.jetbrains.annotations.NotNull;
-
-import java.util.List;
 
 public class ResourceBundleReferenceProvider extends PsiReferenceProvider {
 
