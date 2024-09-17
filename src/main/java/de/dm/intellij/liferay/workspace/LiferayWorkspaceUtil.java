f6968f9cc9513f1bb606fcea415dd036c1df83cd
Dominik Marks <Dominik.Marks@gmx.de>
2024-09-17 11:29:36 +0200

    fix handling liferay.workspace.product not set

--- src/main/java/de/dm/intellij/liferay/workspace/LiferayWorkspaceUtil.java
+++ src/main/java/de/dm/intellij/liferay/workspace/LiferayWorkspaceUtil.java
@@ -6,36 +6,38 @@ public class LiferayWorkspaceUtil {
 
 	//see https://releases.liferay.com/releases.json
 	public static String getLiferayVersion(String liferayWorkspaceProduct) {
-		if (StringUtil.startsWith(liferayWorkspaceProduct, "portal")) {
-			String versionPart = liferayWorkspaceProduct.substring(7);
-			String majorVersion = versionPart.substring(0, versionPart.indexOf("-"));
-			String gaVersion = versionPart.substring(versionPart.indexOf("-"));
+		if (liferayWorkspaceProduct != null) {
+			if (StringUtil.startsWith(liferayWorkspaceProduct, "portal")) {
+				String versionPart = liferayWorkspaceProduct.substring(7);
+				String majorVersion = versionPart.substring(0, versionPart.indexOf("-"));
+				String gaVersion = versionPart.substring(versionPart.indexOf("-"));
 
-			if (StringUtil.equals(majorVersion, "7.4")) {
-				return majorVersion + ".3." + gaVersion.substring(3);
-			} else {
-				return majorVersion + "." + (Integer.parseInt(gaVersion.substring(3)) - 1);
-			}
-		} else if (StringUtil.startsWith(liferayWorkspaceProduct, "dxp")) {
-			String versionPart = liferayWorkspaceProduct.substring(4);
+				if (StringUtil.equals(majorVersion, "7.4")) {
+					return majorVersion + ".3." + gaVersion.substring(3);
+				} else {
+					return majorVersion + "." + (Integer.parseInt(gaVersion.substring(3)) - 1);
+				}
+			} else if (StringUtil.startsWith(liferayWorkspaceProduct, "dxp")) {
+				String versionPart = liferayWorkspaceProduct.substring(4);
 
-			String majorVersion;
-			String updateVersion;
+				String majorVersion;
+				String updateVersion;
 
-			if (versionPart.contains("-")) {
-				majorVersion = versionPart.substring(0, versionPart.indexOf("-"));
-				updateVersion = versionPart.substring(versionPart.indexOf("-") + 1);
-			} else {
-				majorVersion = versionPart.substring(0, 3);
-				updateVersion = versionPart.substring(4);
-			}
+				if (versionPart.contains("-")) {
+					majorVersion = versionPart.substring(0, versionPart.indexOf("-"));
+					updateVersion = versionPart.substring(versionPart.indexOf("-") + 1);
+				} else {
+					majorVersion = versionPart.substring(0, 3);
+					updateVersion = versionPart.substring(4);
+				}
 
-			if (StringUtil.equals(majorVersion, "7.4")) {
-				return majorVersion + ".13." + updateVersion;
-			} else if (StringUtil.startsWith(majorVersion, "7.")) {
-				return majorVersion + ".10." + updateVersion;
-			} else {
-				return versionPart;
+				if (StringUtil.equals(majorVersion, "7.4")) {
+					return majorVersion + ".13." + updateVersion;
+				} else if (StringUtil.startsWith(majorVersion, "7.")) {
+					return majorVersion + ".10." + updateVersion;
+				} else {
+					return versionPart;
+				}
 			}
 		}
 
