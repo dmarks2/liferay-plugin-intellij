c79a7473182a9099659f58c4b14bc4687e92bc24
Dominik Marks <Dominik.Marks@gmx.de>
2025-04-24 15:19:48 +0200

    fix warning

--- testdata/de/dm/intellij/liferay/language/osgi/LiferayOsgiImplicitUsageProviderTest/MyComponent.java
+++ testdata/de/dm/intellij/liferay/language/osgi/LiferayOsgiImplicitUsageProviderTest/MyComponent.java
@@ -1,6 +1,6 @@
 import org.osgi.service.component.annotations.Reference;
 
-public class MyComponent {
+public class <warning descr="Class 'MyComponent' is never used">MyComponent</warning> {
 
     @Reference
     private org.osgi.service.component.annotations.ConfigurationPolicy <warning descr="Private field 'foo' is assigned but never accessed">foo</warning>;
